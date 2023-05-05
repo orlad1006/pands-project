aced1f7516f219cb47dd757a6908f0f7a6451f5e
Wayne O'Brien <orlad1006@gmail.com>
2023-05-05 07:31:55 +0100

    data set

--- analysis.py
+++ analysis.py
@@ -3,5 +3,21 @@
 
 
 #This python file is to perform analysis on the Iris Data set
+# Andrew should be able to run this code 
 
+# First I am going to import the programs that will help me anaylse
+# the data set 
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# to download the data set and add to my git hub respository
+
+filename = 'IrisDataSet.txt'    # df stands for dataframe
+df = pd.read_csv(filename)      # this will read the data in csv format from the .txt 
+
+
+# we will then base all of our analysis from this table.
 
